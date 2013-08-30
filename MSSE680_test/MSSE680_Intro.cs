0b6df675302769b562be47b1ee41b934161c4afc
Andrew Williams <andrew_williams@comcast.net>
2013-08-29 20:45:26 -0600

    new console message

---
+++ MSSE680_test/MSSE680_Intro.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSSE680_test
+{
+    class MSSE680_Intro
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("MSSE680 is underway!");
+            System.Console.ReadLine();
+        }
+    }
+}
